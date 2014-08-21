eb1cd9e4eac36381d3830c1c14ac179e28cde84b
glyyf <toamidan@gmail.com>
2014-08-21 16:28:09 -0500

    Added Stuff
    Added Missle Velocity, Limited it to 2 at a time, and still have no idea
    whats wrong with the muzzle position.

---
+++ Astroids_Test/Assets/Prefab/Projectile_script.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile_script : MonoBehaviour {
+
+	private int tick;
+
+
+	// Use this for initialization
+	void Start () {
+
+		StartCoroutine (WaitAndDestory(5));
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		rigidbody2D.AddForce (new Vector2 (0,5) );
+
+	}
+
+	IEnumerator WaitAndDestory(float time)  {
+
+		yield return new WaitForSeconds (time);
+		Destroy (gameObject);
+		GameObject Player = GameObject.Find ("Player");
+		Player_Controller Script = Player.GetComponent <Player_Controller> ();
+		Script.limit--;
+		
+
+		}
+}
---
+++ Astroids_Test/Assets/Scripts/Player_Controller.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Player_Controller : MonoBehaviour {
+
+	private int tick;
+	public int limit;
+	public GameObject projectile;
+
+	// Use this for initialization
+	void Start () {
+		limit = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (tick % 5 == 0) {
+			if (rigidbody2D.angularVelocity >= 0)
+			rigidbody2D.angularVelocity -= 10;
+
+			if (rigidbody2D.angularVelocity <= 0)
+			rigidbody2D.angularVelocity += 10;
+			}
+
+		rigidbody2D.velocity = new Vector2 (0, 0);
+		float xdirection = Input.GetAxis ("Horizontal");
+		float ydirection = Input.GetAxis ("Vertical"); 
+
+		rigidbody2D.AddTorque (-xdirection);
+		rigidbody2D.AddForce (transform.up * ydirection * 100);
+
+		if (Input.GetButton ("Fire1")) {
+						if (limit == 0 || limit == 1) {
+								FIRe ();
+						}
+				}
+	
+		
+		tick++;
+	}
+
+	public void FIRe () {
+
+		Transform muzzle = GameObject.Find ("Player/Muzzle").transform;
+		 Instantiate (projectile, muzzle.localPosition, Quaternion.identity);
+		limit++;
+
+		}
+}
